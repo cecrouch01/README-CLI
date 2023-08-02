068d4fedda11baf6ffd4d515f67367d11f5af541
Caleb Crouch <cecrouch01@gmail.com>
2023-08-01 22:18:55 -0400

    addition of license being generated with URL

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const questions = [
     {
         type: 'list',
         messages: 'Select a license for your project',
-        choices: ['No License', 'MIT', 'Apache 2.0', 'GNU General Publicv3.0', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'Beerware License'],
+        choices: ['No License', 'MIT', 'Apache 2.0', 'GPL v3.0', 'BSD 2-Clause', 'BSD 3-Clause', 'Boost 1.0', 'CC0', 'EPL 2.0', 'AGPL v3.0', 'GPL v2.0', 'LGPL v2.1', 'MPL 2.0', 'Unlicense', 'Beerware License'],
         name: 'license',
     },
     {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,69 @@
 const { makeBadge, ValidationError } = require('badge-maker')
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+const licenseURL = [
+  {
+    title: 'No License', 
+    url: '',
+  },
+  {
+    title: 'MIT', 
+    url: 'https://opensource.org/licenses/MIT',
+  },
+  {
+    title: 'Apache 2.0', 
+    url: 'https://opensource.org/licenses/Apache-2.0',
+  },
+  {
+    title: 'GPL v3.0', 
+    url: 'https://www.gnu.org/licenses/gpl-3.0',
+  },
+  {
+    title: 'BSD 2-Clause', 
+    url: 'https://opensource.org/licenses/BSD-3-Clause',
+  },
+  {
+    title: 'BSD 3-Clause', 
+    url: 'https://opensource.org/licenses/BSD-2-Clause',
+  },
+  {
+    title: 'Boost 1.0', 
+    url: 'https://www.boost.org/LICENSE_1_0.txt',
+  },
+  {
+    title: 'CC0', 
+    url: 'http://creativecommons.org/publicdomain/zero/1.0/',
+  },
+  {
+    title: 'EPL 2.0', 
+    url: 'https://opensource.org/licenses/EPL-1.0',
+  },
+  {
+    title: 'AGPL v3.0', 
+    url: 'https://www.gnu.org/licenses/agpl-3.0',
+  },
+  {
+    title: 'LGPL v2.1', 
+    url: 'https://www.gnu.org/licenses/lgpl-3.0',
+  },
+  {
+    title: 'GPL v2.0', 
+    url: '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html',
+  },
+  {
+    title: 'MPL 2.0', 
+    url: 'https://opensource.org/licenses/MPL-2.0',
+  },
+  {
+    title: 'Unlicense', 
+    url: 'http://unlicense.org/',
+  },
+  {
+    title: 'Beerware License',
+    url: 'https://www.tldrlegal.com/license/beerware-license',
+  },
+
+]
 function renderLicenseBadge(license) {
   const format = {
     label: 'License',
@@ -20,13 +83,21 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  for(let i =0; i < licenseURL.length; i++){
+    if(license === licenseURL[i].title){
+      return licenseURL[i].url
+    }
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if(license === 'No License'){
+    return ''
+  } else {
+    return `${license}. For more information go to ${renderLicenseLink(license)}`
+  }
 }
 
 // TODO: Create a function to generate markdown for README
@@ -57,8 +128,8 @@ function generateMarkdown(data) {
   ${data.test}
 
   ## License
-  ${data.license}
-
+  ${renderLicenseSection(data.license)}
+ 
   ---
   ## Questions?
   If you have any questions feel free to contact me at ${data.username} or ${data.email}
